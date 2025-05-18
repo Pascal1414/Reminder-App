e8e108da4a90b7f2fed31722d82c440db74334d1
Pascal <pascal.rieder@outlook.com>
2025-05-18 16:52:26 +0200

    Add room dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -45,4 +46,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Room
+    implementation("androidx.room:room-runtime:$2.7.1")  
+    implementation(libs.androidx.room.compiler)
 }
