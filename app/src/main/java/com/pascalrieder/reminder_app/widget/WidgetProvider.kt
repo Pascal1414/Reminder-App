7ef125c105766a3900f077ebf4b3e5cb40663f94
Pascal <pascal.rieder@outlook.com>
2025-05-21 13:22:10 +0200

    Update WidgetProvider

--- app/src/main/java/com/pascalrieder/reminder_app/widget/WidgetProvider.kt
+++ app/src/main/java/com/pascalrieder/reminder_app/widget/WidgetProvider.kt
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.widget.RemoteViews
 import com.pascalrieder.reminder_app.R
 
@@ -21,21 +22,33 @@ class WidgetProvider : AppWidgetProvider() {
         appWidgetIds.forEach { appWidgetId ->
             val views = RemoteViews(context.packageName, R.layout.widget)
 
-            val intent = Intent(context, WidgetProvider::class.java).apply {
-                action = TOGGLE_ACTION
-            }
+            val reminderId = WidgetConfigActivity.getReminderId(context, appWidgetId)
+            views.setTextViewText(R.id.widget_check_reminder_text, reminderId.toString())
 
-            val pendingIntent = PendingIntent.getBroadcast(
-                context,
-                0,
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-            )
+            setupToggleReceiver(context, views)
 
-            views.setOnClickPendingIntent(R.id.widget_check_reminder, pendingIntent)
-
+            // Has to be called after setting up the view
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
+    }
+
+    private fun setupToggleReceiver(
+        context: Context,
+        views: RemoteViews,
+    ) {
+
+        val intent = Intent(context, WidgetProvider::class.java).apply {
+            action = TOGGLE_ACTION
+        }
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        views.setOnClickPendingIntent(R.id.widget_check_reminder, pendingIntent)
     }
 
     override fun onReceive(context: Context, intent: Intent) {
