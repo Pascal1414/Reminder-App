9355a27707c1ba53cdfc53d1a905aac4bd155831
Pascal <pascal.rieder@outlook.com>
2025-05-21 11:49:18 +0200

    Refactor

--- app/src/main/java/com/pascalrieder/reminder_app/widget/CheckReminderWidgetConfigurationActivity.kt
+++ app/src/main/java/com/pascalrieder/reminder_app/widget/CheckReminderWidgetConfigurationActivity.kt
@@ -1,10 +1,10 @@
-package com.pascalrieder.todotracker.widget
+package com.pascalrieder.reminder_app.widget
 
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class CheckTodoWidgetConfigurationActivity : AppCompatActivity() {
+class CheckReminderWidgetConfigurationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, "Configuration activity started", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/pascalrieder/reminder_app/widget/CheckReminderWidgetProvider.kt
+++ app/src/main/java/com/pascalrieder/reminder_app/widget/CheckReminderWidgetProvider.kt
@@ -1,4 +1,4 @@
-package com.pascalrieder.todotracker.widget
+package com.pascalrieder.reminder_app.widget
 
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
@@ -6,9 +6,9 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
-import com.pascalrieder.todotracker.R
+import com.pascalrieder.reminder_app.R
 
-class CheckTodoWidgetProvider : AppWidgetProvider() {
+class CheckReminderWidgetProvider : AppWidgetProvider() {
     companion object {
         const val TOGGLE_ACTION = "com.pascalrieder.TOGGLE_ACTION"
     }
@@ -19,9 +19,9 @@ class CheckTodoWidgetProvider : AppWidgetProvider() {
         appWidgetIds: IntArray
     ) {
         appWidgetIds.forEach { appWidgetId ->
-            val views = RemoteViews(context.packageName, R.layout.widget_check_todo)
+            val views = RemoteViews(context.packageName, R.layout.widget_check_reminder)
 
-            val intent = Intent(context, CheckTodoWidgetProvider::class.java).apply {
+            val intent = Intent(context, CheckReminderWidgetProvider::class.java).apply {
                 action = TOGGLE_ACTION
             }
 
@@ -32,7 +32,7 @@ class CheckTodoWidgetProvider : AppWidgetProvider() {
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
 
-            views.setOnClickPendingIntent(R.id.widget_check_todo, pendingIntent)
+            views.setOnClickPendingIntent(R.id.widget_check_reminder, pendingIntent)
 
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
@@ -42,7 +42,7 @@ class CheckTodoWidgetProvider : AppWidgetProvider() {
         super.onReceive(context, intent)
 
         if (intent.action == TOGGLE_ACTION) {
-           // TODO: Toggle the todo state
+            // TODO: Toggle the reminder state
         }
     }
 }
